0d8576de814b8ed6f0697a0e15de3c227de39cfe
AyalaKelerman <a6771435@gmail.com>
2024-07-03 21:03:54 +0300

    updateMeetingAndService

--- Controllers/meeting.controller.js
+++ Controllers/meeting.controller.js
@@ -3,34 +3,59 @@ const router = express.Router();
 
 
 router.get('/meetings', async (req, res) => {
-    const meetings = await meetingService.getMeetings();
-    res.send(meetings);
+    try {
+        const meetings = await meetingService.getMeetings();
+        res.send(meetings);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 router.get('/meetings/:orderNumber', async (req, res) => {
-    const orderNumber = req.params.orderNumber;
-    const meeting = await meetingService.getMeeting(orderNumber);
-    if (meeting)
-        res.json(meeting);
-    else
-        res.status(404).json({ message: 'this order number not exist' });
+    try {
+        const orderNumber = req.params.orderNumber;
+        const meeting = await meetingService.getMeeting(orderNumber);
+        if (meeting)
+            res.json(meeting);
+        else
+            res.status(404).json({ message: 'this order number not exist' });
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
-router.post('/meetings', async (req,res)=>{
-    const newMeeting = req.body;
-    await meetingService.createMeeting(newMeeting);
-    res.status(201).json(newMeeting);
+router.post('/meetings', async (req, res) => {
+    try {
+        const newMeeting = req.body;
+        await meetingService.createMeeting(newMeeting);
+        res.status(201).json(newMeeting);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
-router.put('/meetings/:orderNumber', async (req,res) =>{
-    const orderNumber = req.params.orderNumber;
-    const updateMeeting = req.body;
-    await meetingService.updateMeeting(updateMeeting);
-    res.json(updateMeeting);
+router.put('/meetings/:orderNumber', async (req, res) => {
+    try {
+        const orderNumber = req.params.orderNumber;
+        const updateMeeting = req.body;
+        await meetingService.updateMeeting(updateMeeting);
+        res.json(updateMeeting);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
-router.delete('/meetings/:orderNumber', async (req,res)=>{
-    const orderNumber = req.params.orderNumber;
-    await meetingService.delete(orderNumber);
-    res.status(200).json({message: 'deleted'})
+router.delete('/meetings/:orderNumber', async (req, res) => {
+    try {
+        const orderNumber = req.params.orderNumber;
+        await meetingService.delete(orderNumber);
+        res.status(200).json({ message: 'deleted' })
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
--- Controllers/service.controller.js
+++ Controllers/service.controller.js
@@ -3,34 +3,60 @@ const router = express.Router();
 
 
 router.get('/services', async (req, res) => {
-    const services = await serviceService.getServices();
-    res.send(services);
+    try {
+        const services = await serviceService.getServices();
+        res.send(services);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 router.get('/services/:id', async (req, res) => {
-    const id = req.params.id;
-    const service = await serviceService.getService(id);
-    if (service)
-        res.json(service);
-    else
-        res.status(404).json({ message: 'this id not exist' });
+    try {
+        const id = req.params.id;
+        const service = await serviceService.getService(id);
+        if (service)
+            res.json(service);
+        else
+            res.status(404).json({ message: 'this id not exist' });
+    }
+    catch (error) {
+        res.status(500).json(error.message)
+    }
 });
 
-router.post('/services', async (req,res)=>{
-    const newService = req.body;
-    await serviceService.createService(newService);
-    res.status(201).json(newService);
+router.post('/services', async (req, res) => {
+    try {
+
+        const newService = req.body;
+        await serviceService.createService(newService);
+        res.status(201).json(newService);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
-router.put('/services/:id', async (req,res) =>{
-    const id = req.params.id;
-    const updateService = req.body;
-    await serviceService.updateService(updateService);
-    res.json(updateService);
+router.put('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updateService = req.body;
+        await serviceService.updateService(updateService);
+        res.json(updateService);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
-router.delete('/services/:id', async (req,res)=>{
-    const id = req.params.id;
-    await serviceService.delete(id);
-    res.status(200).json({message: 'deleted'})
+router.delete('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await serviceService.delete(id);
+        res.status(200).json({ message: 'deleted' })
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
